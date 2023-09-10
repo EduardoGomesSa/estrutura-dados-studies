2a0d9a2e6d2963b7214ab092f6bd51bf54e67444
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-09-09 22:28:30 -0300

    criação do projeto java e da classe vetor

---
+++ estrutura-dados-java/src/com/eduardo/estruturadados/vetor/Vetor.java
@@ -0,0 +1,9 @@
+package com.eduardo.estruturadados.vetor;
+
+public class Vetor {
+    private String[] elementos;
+
+    public Vetor(int capacidade){
+        this.elementos = new String[capacidade];
+    }
+}
