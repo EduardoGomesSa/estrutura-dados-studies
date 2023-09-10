6e28b6fe7010b137c6eec6d2403ea4fbb872575c
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-09-09 22:50:55 -0300

    criação de um método alternativo usando uma variavél tamanho para encontrar espaços vazios

--- estrutura-dados-java/src/com/eduardo/estruturadados/vetor/Vetor.java
+++ estrutura-dados-java/src/com/eduardo/estruturadados/vetor/Vetor.java
@@ -2,17 +2,28 @@ package com.eduardo.estruturadados.vetor;
 
 public class Vetor {
     private String[] elementos;
+    private int tamanho;
 
     public Vetor(int capacidade){
         this.elementos = new String[capacidade];
+        this.tamanho = 0;
     }
 
-    public void adiciona(String elemento){
+    /*public void adiciona(String elemento){
         for(int i = 0; i < elementos.length; i++){
             if(elementos[i] == null){
                 elementos[i] = elemento;
                 break;
             }
+        }
+    } */
+
+    public void adiciona(String elemento) throws Exception{
+        if(tamanho < elementos.length){
+            this.elementos[tamanho] = elemento;
+            this.tamanho++;
+        } else {
+            throw new Exception("Tamanho do vetor insuficiente para novas adições");
         }
     }
 }
--- estrutura-dados-java/src/com/eduardo/estruturadados/vetor/teste/Aula03.java
+++ estrutura-dados-java/src/com/eduardo/estruturadados/vetor/teste/Aula03.java
@@ -4,10 +4,15 @@ import com.eduardo.estruturadados.vetor.Vetor;
 
 public class Aula03 {
     public static void main(String[] args){
-        Vetor vetor = new Vetor(5);
+        Vetor vetor = new Vetor(2);
 
-        vetor.adiciona("elemento 1");
-        vetor.adiciona("elemento 2");
+        try {
+            vetor.adiciona("elemento 1");
+            vetor.adiciona("elemento 2");
+            vetor.adiciona("elemento 3");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
         int i = 0;
         i++;
