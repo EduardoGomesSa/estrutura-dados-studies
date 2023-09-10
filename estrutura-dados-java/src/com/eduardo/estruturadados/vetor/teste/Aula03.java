83eb7d97565aebd5790c0d0fbe6088edb9a23a16
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-09-09 22:40:01 -0300

    criação do método adiciona

---
+++ estrutura-dados-java/src/com/eduardo/estruturadados/vetor/teste/Aula03.java
@@ -0,0 +1,15 @@
+package com.eduardo.estruturadados.vetor.teste;
+
+import com.eduardo.estruturadados.vetor.Vetor;
+
+public class Aula03 {
+    public static void main(String[] args){
+        Vetor vetor = new Vetor(5);
+
+        vetor.adiciona("elemento 1");
+        vetor.adiciona("elemento 2");
+
+        int i = 0;
+        i++;
+    }
+}
