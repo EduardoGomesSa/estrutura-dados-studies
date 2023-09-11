5666c9db85c0ab8de9b6aa78fef514ac044e9b59
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-09-11 11:00:07 -0300

    criação de uma class contato para ser adicionada na class vetoobjetos

--- estrutura-dados-java/src/com/eduardo/estruturadados/vetor/teste/Aula10.java
+++ estrutura-dados-java/src/com/eduardo/estruturadados/vetor/teste/Aula10.java
@@ -6,10 +6,24 @@ public class Aula10 {
     public static void main(String[] args){
         VetorObjetos vetor = new VetorObjetos(3);
 
-        vetor.adiciona(2);
-        vetor.adiciona(3);
-        vetor.adiciona(4);
+        Contato c1 = new Contato("contato 1", "123-456", "contato1@gmail.com");
+        Contato c2 = new Contato("contato 2", "321-546", "contato2@gmail.com");
+        Contato c3 = new Contato("contato 3", "213-654", "contato3@gmail.com");
+
+        Contato c4 = new Contato("contato 3", "213-654", "contato3@gmail.com");
+
+        vetor.adiciona(c1);
+        vetor.adiciona(c2);
+        vetor.adiciona(c3);
 
         System.out.println(vetor);
+
+        int pos = vetor.busca(c4);
+
+        if(pos > -1){
+            System.out.println("Elemento existe no vetor");
+        }else {
+            System.out.println("Elemento não existe no vetor");
+        }
     }
 }
