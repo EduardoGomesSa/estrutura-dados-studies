6fad13cbe8271f37f7089c1388e09656377a003e
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-09-10 22:52:41 -0300

    criação do método de adicionar elemento em qualquer posição sem perder o antigo valor

--- estrutura-dados-java/src/com/eduardo/estruturadados/vetor/Vetor.java
+++ estrutura-dados-java/src/com/eduardo/estruturadados/vetor/Vetor.java
@@ -31,6 +31,22 @@ public class Vetor {
         return false;
     }
 
+    public boolean adiciona(int posicao, String elemento){
+        if(!(posicao >= 0 && posicao < this.tamanho)){
+            throw new IllegalArgumentException("Posição inválida");
+        }
+
+        // mover todos os elementos
+        for(int i = this.tamanho-1; i >= posicao; i--){
+            this.elementos[i+1] = this.elementos[i];
+        }
+
+        this.elementos[posicao] = elemento;
+        this.tamanho++;
+
+        return true;
+    }
+
     public String busca(int posicao){
         if(!(posicao >= 0 && posicao < this.tamanho)){
             throw new IllegalArgumentException("Posição inválida");
