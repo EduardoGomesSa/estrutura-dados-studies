ed3b74b10a6cafda0933af1a33e3ed2d96e60f30
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-10-18 17:13:08 -0300

    criação do metodo empilhar

---
+++ estrutura-dados-java/src/pilha/teste/Aula14.java
@@ -0,0 +1,16 @@
+package pilha.teste;
+
+import pilha.Pilha;
+
+public class Aula14 {
+    public static void main(String[] args){
+        Pilha<Integer> pilha = new Pilha<Integer>();
+
+        for (int i=0; i<=10; i++){
+            pilha.empilha(i);
+        }
+
+        System.out.println(pilha);
+        System.out.println(pilha.tamanho());
+    }
+}
