f5130b1c2535c745db44683a7f033e6d45113090
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-11-02 17:07:48 -0300

    criação da classe listaencadeada e da função de tamanho

---
+++ estrutura-dados-java/src/lista/ListaEncadeada.java
@@ -0,0 +1,24 @@
+package lista;
+
+public class ListaEncadeada<T> {
+    private No<T> inicio;
+    private int tamanho = 0;
+
+    public void adiciona(T elemento){
+        No<T> celula = new No<>(elemento);
+        this.inicio = celula;
+
+        this.tamanho++;
+    }
+
+    public int getTamanho(){
+        return this.tamanho;
+    }
+
+    @Override
+    public String toString() {
+        return "ListaEncadeada{" +
+                "inicio=" + inicio +
+                '}';
+    }
+}
---
+++ estrutura-dados-java/src/lista/teste/ListaEncadeadaTeste.java
@@ -0,0 +1,13 @@
+package lista.teste;
+
+import lista.ListaEncadeada;
+
+public class ListaEncadeadaTeste {
+    public static void main(String[] args) {
+        ListaEncadeada<Integer> lista = new ListaEncadeada<Integer>();
+        lista.adiciona(1);
+
+        System.out.println("Tamanho: "+lista.getTamanho());
+        System.out.println(lista);
+    }
+}
