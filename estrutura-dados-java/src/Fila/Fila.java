8d32f2de52671809db57273bd5c43b2769c8d399
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-11-02 01:52:08 -0300

    criação da class Fila

---
+++ estrutura-dados-java/src/Fila/Fila.java
@@ -0,0 +1,13 @@
+package Fila;
+
+import base.EstruturaEstatica;
+
+public class Fila<T> extends EstruturaEstatica<T> {
+    public Fila(){
+        super();
+    }
+
+    public Fila(int capacidade){
+        super(capacidade);
+    }
+}
---
+++ estrutura-dados-java/src/Fila/teste/Aula19.java
@@ -0,0 +1,12 @@
+package Fila.teste;
+
+import Fila.Fila;
+
+public class Aula19 {
+    public static void main(String[] args) {
+        Fila<Integer> fila = new Fila<Integer>();
+
+        System.out.println(fila.estaVazia());
+        System.out.println(fila.tamanho());
+    }
+}
