7bcfb575bbf526b05b04f82d3e1bc47cf93831b6
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-11-02 17:27:15 -0300

    criação do método inserir no final

--- estrutura-dados-java/src/lista/teste/ListaEncadeadaTeste.java
+++ estrutura-dados-java/src/lista/teste/ListaEncadeadaTeste.java
@@ -6,6 +6,8 @@ public class ListaEncadeadaTeste {
     public static void main(String[] args) {
         ListaEncadeada<Integer> lista = new ListaEncadeada<Integer>();
         lista.adiciona(1);
+        lista.adiciona(2);
+        lista.adiciona(3);
 
         System.out.println("Tamanho: "+lista.getTamanho());
         System.out.println(lista);
