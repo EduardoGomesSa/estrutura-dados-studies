66c8126eecc24027e45f4a03a46f173f28e7980e
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-11-02 16:09:14 -0300

    testanto fila com prioridade

---
+++ estrutura-dados-java/src/Fila/teste/Aula24.java
@@ -0,0 +1,15 @@
+package Fila.teste;
+
+import Fila.FilaComPrioridade;
+
+public class Aula24 {
+    public static void main(String[] args) {
+        FilaComPrioridade<Integer> fila = new FilaComPrioridade<Integer>();
+
+        fila.enfileira(1);
+        fila.enfileira(3);
+        fila.enfileira(2);
+
+        System.out.println(fila);
+    }
+}
