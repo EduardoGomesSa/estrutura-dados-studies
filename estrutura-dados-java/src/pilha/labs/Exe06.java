c620b076b43d897a3f0575fcfb14a3ceb62a835e
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-11-01 23:22:33 -0300

    correção do exercicio 05

---
+++ estrutura-dados-java/src/pilha/labs/Exe06.java
@@ -0,0 +1,44 @@
+package pilha.labs;
+
+import pilha.Pilha;
+
+public class Exe06 {
+    public static void main(String[] args) {
+        String palavra = "ada";
+        imprimeResultado(palavra);
+
+        palavra = "adam";
+        imprimeResultado(palavra);
+
+        palavra = "socorram me subi no onibus em marrocos";
+        imprimeResultado(palavra);
+
+        palavra = "aaa";
+        imprimeResultado(palavra);
+
+    }
+
+    public static void imprimeResultado(String palavra){
+        System.out.println(palavra + " é um palindromo? "+testaPalindromo(palavra));
+    }
+
+    public static boolean testaPalindromo(String palavra){
+        Pilha<Character> pilha = new Pilha<>();
+
+        for (int i=0; i<palavra.length(); i++){
+            pilha.empilha(palavra.charAt(i));
+        }
+
+        String palavraInversa = "";
+
+        while (!pilha.estaVazia()){
+            palavraInversa += pilha.desempilha();
+        }
+
+        if(palavraInversa.equalsIgnoreCase(palavra)){
+            return true;
+        }
+
+        return false;
+    }
+}
