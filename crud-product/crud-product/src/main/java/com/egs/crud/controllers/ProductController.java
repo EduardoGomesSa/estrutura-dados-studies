3e10bc34619367389081f89cfe43b5e773315364
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-11-07 17:47:36 -0300

    ligação com a base de dados postgres funcionando

--- crud-product/crud-product/src/main/java/com/egs/crud/controllers/ProductController.java
+++ crud-product/crud-product/src/main/java/com/egs/crud/controllers/ProductController.java
@@ -1,7 +1,17 @@
 package com.egs.crud.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("products")
+@RequiredArgsConstructor
 public class ProductController {
+    @GetMapping
+    public String getAll(){
+        return "quero chorarrrrrr";
+    }
 }
