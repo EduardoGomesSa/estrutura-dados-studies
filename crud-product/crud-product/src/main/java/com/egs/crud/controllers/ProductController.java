c0c9992de603b82d2662c4ebf7499ac18b005ac3
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-11-07 14:53:08 -0300

    criação das classes do projeto

---
+++ crud-product/crud-product/src/main/java/com/egs/crud/controllers/ProductController.java
@@ -0,0 +1,7 @@
+package com.egs.crud.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ProductController {
+}
---
+++ crud-product/crud-product/src/main/java/com/egs/crud/models/Product.java
@@ -0,0 +1,47 @@
+package com.egs.crud.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private Long id;
+    private String name;
+    private double preco;
+    private int quantidade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
